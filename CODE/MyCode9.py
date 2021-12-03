ca31d526bf82e2dff8aa7e8d34740865633b4f09
Andraelity <Andraelity@gmail.com>
2021-12-03 14:16:13 -0500

    Second commit

---
+++ CODE/MyCode9.py
@@ -0,0 +1,209 @@
+#Como yo puedo encaminar en la realidad las rutas que promueven como mi saber se genera, como mi conocimiento humano se hace mas evolutivo, como la totalidad hace que la experiencia 
+#humana sea mas alta.
+
+
+
+#Esto esta muy bueno, es el saber el que me permite analizar como las rutas de la totalidad mejoran como mi personalidad se vive, como mi existencia se formula, como mi expresion narrativa
+#Se vive.
+
+
+
+#Ese momento donde puedo amplificar como las acciones de mi mente determinan como la totalidad se produce, como mi experiencia interna se vive de maneras mas practicas, como yo puedo evolucionar con tal de entender, 
+#El concepto de mi vida, como yo puedo entender, que realidad se vive en mi.
+
+
+#El movimiento que me permite analizar las rutas del saber que me direccionan hacia ese comportamiento que produce mente, el tipo de realidad que produce vida, las acciones de la mente
+#Que me determinan como persona.
+
+
+#llenarte de tantas expresiones de la mente que toda tu realidad simplemente se supere a si misma, ante el detalle de semejante expresion.
+
+#Me vas a hacer evolucionar en el ahora de maneras mas claras como se puede vivir la realidad al grado mas nuevo, como yo puedo determinar el comportamiento que me hace mas supremo.
+
+
+#Aprendiendo como el saber se puede hacer mas claro como el movimiento de mi totalidad maximiza como mi mente se puede construir, como las propiedades de mi presente mejoran como yo puedo ser
+
+#Narrando como las propiedades de mi conciencia maximizan como el saber se produce, como el movimiento de mi mente eleva cuales expresiones yo quiero vivir, que tipo de procesos yo quiero formar.
+
+
+
+#Esta expresion sucede de maneras muy particulares, como el componente narrativo se vive en mi conciencia, como yo puedo expresar en el saber las conexiones que yo quiero experimentar.
+
+
+#Narrando las rutas que suceden en mi.
+
+
+
+#Como yo le meto a los juegos, como yo puedo seguir procesando las acciones que producen el comportamiento que yo quiero hacer verdad, las rutas del saber que motivan como mi identidad se genera.
+
+
+
+import os
+
+os.system('dir')
+pathTesting = os.getcwd()
+
+
+#os.system('cd d:\\ \ndir')
+os.system('dir')
+
+
+
+
+print()
+print()
+print()
+
+os.chdir('d:\\')
+pathTesting = os.getcwd()
+print(pathTesting)
+
+os.chdir('D:\INTERNET\Wiki-1-THEGAME-01\\')
+
+pathTesting = os.getcwd()
+
+print()
+logString = os.system('git log -n 1')
+
+print(logString)
+
+print(pathTesting)
+
+#Mire como nostoros le vamos a hacer a esto.
+#Esto es una expresion.
+#Una expresion qeu nosotros podemos sintetizar.
+#Como podemos hacer para combinar
+# 
+#Yo estaba haciendo el siguiente movimiento.
+#El componente de crear codigo, el componente de crear mentalidad, 
+# Como aprovechar las interacciones del codigo con el almacenamiento.
+#Como almacenar mas codigo, como almacenar mas opciones, como 
+#Detallar el comportamiento que se produce a todo momento.
+
+#Crear el comportamiento que define el almacenamiento en la nube
+#Ese almacenamiento sucede cuando se pueden generar procesos
+#Del tipo, creativo computacional, la habilidad de producir mejores
+#sucesos en el momento presente, como valorar las rutas de nuestro 
+#presente.
+
+
+#Como le metemos a esto.
+
+
+#Que yo sepa que a todo momento va subir.
+#Que paso.
+
+
+#Que le tiramos al codigo.
+
+#Siempre sepa que el codigo que yo vaya a tirar funcione.
+
+#Como le podemos hacer a la mente para que sea mas clara.
+
+
+#Como yo le puedo hacer a eso.
+#Yo le hago eso a mi vida cuando organizo las siguiente funciones.
+#Como yo puedo producir las rutas que se mueven en mi.
+#Como se pueden organizar las relaciones los poderes que producen.
+
+
+#lim x->(imagenes-texto).
+
+#Texto en .extension, que compile siempre hacia txt.
+
+#El programa que me lanza siempre a guardar libros, y programas, 
+#y puras paginas de texto con imagenes.
+
+#Como me lo vas a guardar, me lo vas a guardar en gitHub.
+#Que sepa yo que siempre guarda.
+#Que siempre guarda en la nubecita github
+#Como yo puedo configurar estas acciones.
+#Estos sistemas.
+
+#Como yo produzco las interacciones qeu modelan como la totalidad 
+#se produce.
+#Como el comportamiento de las narrativas alcanza accion.
+#Como yo puedo conocer las mentes.
+#COmo yo puedo entender las realidades.
+#Como yo puedo aprovecharme de la realidad.
+#La idea seria sintetizar las siguiente expresion.
+
+
+#Como le tiramos al compilador.
+#TOdo esta funcionando en razon de la eficiencia, de que de su capacidad
+#De producir mas emocion en usted.
+
+
+
+
+#Me vas a mandar a C# a tirar burst compiler.
+#porque compila en todas las maquinas.
+#Compila en todos los sitios
+#De que forma.
+
+#Eso es lo unico que se necesita, el factor que produce cuando componemos
+#Los factores que producen cambio.
+
+
+
+
+#Terminal
+
+#Como yo puedo analizar las propiedades de mi mente, como yo puedo formular todos esos activos de mi mente
+#Como yo puedo dilucidar las perspectivas que se mueven en mi, como yo puedo concebir la realidad en su mas alta forma.
+#Como yo puedo sintetizar las versiones que se producen en mi, mejorando como el saber se crea en mi mente, como mi vida puede 
+#Ser mas nueva, como yo puedo analizar las relaciones con el todo.
+
+
+#Ponme a crear juegitos, como yo puedo crear jueguitos mandame a crear jueguitos.
+
+
+#Me vas a sacar el promedio de los valores
+
+
+#Como le hago para producir los factores que yo quiero procesar, las interacciones que yo quiero hacer verdad, todos esos componentes que motivan como mi 
+#Concepto interno se mejora.
+
+
+#
+
+
+                   
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ CODE/THEGAME.py
@@ -0,0 +1,32 @@
+""" 
+El compilador maneja memoria, algoritmos, entrada y salida.
+
+Como yo le aprendo al codigo yo le aprendo al codigo.
+
+
+
+Yo y la maquina todo el tiempo, yo y el elemento que prodoceu voltaje 
+
+"""
+
+
+#Como se pueden analizar las rutas de la totalidad qeu motivan como el desarrollo de la mente se puede comprender, como yo puedo construir el factor que me ayuda a procesar las acciones del saber a un grado mas alto
+#Como yo puedo determinar en el ahora como las rutas de la vida hacen qeu mi saber sea mas claro como yo puedo organizar las versiones que me ayudan a enfocar 
+
+
+
+#Como se pueden mejorar las caracteristicas que me permiten definir como el comportamiento del ahora hacen que los productos 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
